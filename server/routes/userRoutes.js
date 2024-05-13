1285d5ac2a23ec54301939f95045b8d1ac39202d
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2024-05-13 19:04:48 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const userController = require("./../controllers/userController");
 const authController = require("./../controllers/authController");
 
 const router = express.Router();
