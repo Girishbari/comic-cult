f20dbb5297aecf6f23cb70154b32fa1609ac8ac0
prabh1234assla <pdsinghassla@gmail.com>
2024-05-20 23:27:09 +0530

    AUTHController has been updated
    - exports.signup is not converted to default export as we still haven't seen any definition of catchAsync within our file
    - signToken function also has no definition within our file

--- apps/server/controllers/authController.ts
+++ apps/server/controllers/authController.ts
@@ -1,64 +1,68 @@
 const User = {}; // Change after the model decided for the user
+import { NextFunction, Response } from "express";
 import jwt from "jsonwebtoken";
 
-interface CookieOptions{
-  expires: Date
-  httpOnly: boolean
-  secure: boolean
+interface CookieOptions {
+  expires: Date;
+  httpOnly: boolean;
+  secure: boolean;
 }
 
 //don't know what ResponseType is so we are assuming as ResponseType
-interface ResponseType{
-  cookie: (type: "jwt" | "others", token: string, cookieOptions: CookieOptions) => void
+interface ResponseType {
+  cookie: (
+    type: "jwt" | "others",
+    token: string,
+    cookieOptions: CookieOptions
+  ) => void;
 }
 
-
-interface User{
-  password?: string | undefined
-  _id: string
+interface User {
+  password?: string | undefined;
+  _id: string;
 }
 
 const createSendToken: (
   user: User,
   statusCode: number,
-  res: ResponseType
+  res: Response
 ) => void = (user, statusCode, res) => {
-
-
+  
+  // no definition of signToken present
   const token = signToken("replace with user id on db"); // Change after the model decided for the user
 
-  if(process.env && process.env.JWT_COOKIE_EXPIRES_IN){
-
+  if (process.env && process.env.JWT_COOKIE_EXPIRES_IN) {
     const cookieOptions: CookieOptions = {
       expires: new Date(
-        Date.now() + parseInt(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000
+        Date.now() +
+          parseInt(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      secure: false
+      secure: false,
     };
-    
+
     if (process.env.NODE_ENV === "production") {
       cookieOptions.secure = true;
     }
 
     res.cookie("jwt", token, cookieOptions);
-  
+
     // Remove password from output
     user.password = undefined;
-    
+
     res.status(statusCode).json({
       status: "success",
       token,
       data: {
-        user
+        user,
       },
     });
-
   }
 };
 
-export default signup = function catchAsync(async (req: undefined, res: ResponseType, next: undefined) => {
-  const newUser: User = { _id: "new user id" }; // Replace with new user 
+//no definition of catchAsync present
+exports.signup = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const newUser = { _id: "new user id" }; // Replace with new user object
 
   if(process.env && process.env.JWT_SECRET){
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
