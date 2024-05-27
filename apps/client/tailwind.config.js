c588da6344d43a960231189e62b8c895d4717257
kart2004 <karthikprakash999@gmail.com>
2024-05-27 19:16:21 +0530

    Move app/components to packages

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
     content: [
       "./app/**/*.{js,ts,jsx,tsx,mdx}",
-      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      //"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./packages/**/*.{js,ts,jsx,tsx,mdx}",
       "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
