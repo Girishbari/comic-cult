7a99d770a9955579e0aa7e2c0cf512dd4fac2082
kart2004 <karthikprakash999@gmail.com>
2024-05-27 19:19:49 +0530

    app/components to packages

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     content: [
       "./app/**/*.{js,ts,jsx,tsx,mdx}",
-      //"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
       "./packages/**/*.{js,ts,jsx,tsx,mdx}",
       "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
